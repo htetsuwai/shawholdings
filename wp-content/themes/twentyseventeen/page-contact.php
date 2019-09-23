d48d9174d8874f890b563297cb232ef3b0d030aa
Zan <htetsuwai@gmail.com>
2019-09-23 17:00:55 +0630

    added files

---
+++ wp-content/themes/twentyseventeen/page-contact.php
@@ -0,0 +1,28 @@
+<?php
+ /**
+  Template name: contact-us
+ */
+?>
+
+<?php get_header(); ?>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) :
+				the_post(); ?>
+
+				<?php  the_content(); ?>
+
+			<?php 
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<?php
+get_footer();
